a9d31a8864beeed51db086c89e3ea3d67f7fab9a
okabe-yuya <takamizawa46@gmail.com>
2023-10-15 13:15:43 +0900

    feat: リンクの構文をサポート
    リンクを貼れるようになりました。

---
+++ parser/parse_link.go
@@ -0,0 +1,32 @@
+package parser
+
+import (
+	"fmt"
+
+	"github.com/okabe-yuya/makrdown-to-html/lexer"
+)
+
+func parseLink(token *lexer.Token) (*Node, *lexer.Token) {
+	var node *Node
+	display, link := "", ""
+	curToken := token.Next
+
+	for curToken != nil && !exepct(curToken, lexer.RESERVED, "]") {
+		display += curToken.Value
+		curToken = curToken.Next
+	}
+	if expectNext(curToken, lexer.RESERVED, "(") {
+		curToken = seek(curToken, 2) // ]と(の2つ分
+		for curToken != nil && !exepct(curToken, lexer.RESERVED, ")") {
+			link += curToken.Value
+			curToken = curToken.Next
+		}
+		node = NewNode(ND_LINK, link, 0, 0)
+		node.Nest = NewNode(ND_VALUE, display, 0, 0)
+		curToken = curToken.Next // )分
+	} else {
+		node, curToken = parseText(curToken.Next)
+		node.Value = fmt.Sprintf("[%s%s", display, node.Value)
+	}
+	return node, curToken
+}
